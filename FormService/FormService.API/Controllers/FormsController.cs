d60d910b15b3d26af6ea7544658d5a8bdb6379c6
Kadir Gultekin <kadirgulteekin@gmail.com>
2024-09-10 23:48:52 +0300

    FormServiceDomain created and project references configuration

---
+++ FormService/FormService.API/Controllers/FormsController.cs
@@ -0,0 +1,17 @@
+﻿using FormService.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FormsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public FormsController()
+        {
+             
+        }
+    }
+}
