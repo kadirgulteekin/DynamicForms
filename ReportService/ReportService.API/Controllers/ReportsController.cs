175e508b99d6fd4c9ddbc59c878f9d605efbd47e
Kadir Gultekin <kadirgulteekin@gmail.com>
2024-09-11 01:17:20 +0300

    GetFormReport

--- ReportService/ReportService.API/Controllers/ReportsController.cs
+++ ReportService/ReportService.API/Controllers/ReportsController.cs
@@ -1,6 +1,7 @@
 ﻿using FormService.Infrastructure;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ReportService.API.Controllers
 {
@@ -9,5 +10,25 @@ namespace ReportService.API.Controllers
     public class ReportsController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+
+        public ReportsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("form-report")]
+        public async Task<IActionResult> GetFormReport()
+        {
+            var formReport = await _context.Forms
+                .Select(f => new
+                {
+                    f.FormName,
+                    f.FormDescription,
+                    DataCount = _context.FormData.Count(d => d.FormId == f.UUID)
+                })
+                .ToListAsync();
+
+            return Ok(formReport);
+        }
     }
 }
