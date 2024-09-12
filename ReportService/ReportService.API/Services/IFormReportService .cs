bcc4e55f9206feea9aed7d0570d47f5c714f19ab
Kadir Gultekin <kadirgulteekin@gmail.com>
2024-09-12 15:16:45 +0300

    Using Single Responsibility Principle (SRP) IFormReportService and Controller

---
+++ ReportService/ReportService.API/Services/IFormReportService .cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReportService.API.Services
+{
+    public interface IFormReportService
+    {
+        Task<List<object>> GetFormReportAsync();
+
+        Task<List<Dictionary<string, string>>> GetFormDataReport(Guid id);
+    }
+}
